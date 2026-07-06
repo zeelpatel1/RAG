c61cb5bc78c227956d2184dd661878e76497fc4e
zeelpatel1 <zeelpatel6878@gmail.com>
2026-07-06 15:13:05 +0530

    implemented textLoader

---
+++ document loaders/test.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+
+data=TextLoader("document loaders/notes.txt")
+docs=data.load()
+
+print(docs )
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
+from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
+data=TextLoader("document loaders/notes.txt")
+docs=data.load()
+
+template=ChatPromptTemplate.from_messages(
+    [("system","you are a AI that summerizes the text"),("human","{data}")]
+)
+
+prompt=template.invoke({
+    "data": docs[0].page_content
+})
+
 model=ChatMistralAI(model="mistral-small-2506")
-res=model.invoke("hello")
+
+res=model.invoke(prompt)
 print(res.content)
